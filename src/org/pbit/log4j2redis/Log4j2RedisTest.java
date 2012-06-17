fab00176796cb093676dd0844f233d660ab29db9
pavlobaron <pb@pbit.org>
2012-06-17 09:47:48 +0200

    initial

---
+++ src/org/pbit/log4j2redis/Log4j2RedisTest.java
@@ -0,0 +1,24 @@
+package org.pbit.log4j2redis;
+
+import org.apache.log4j.Logger;
+
+public class Log4j2RedisTest {
+    
+    public static class LogThread extends Thread {
+        Logger log = Logger.getLogger("LogThread");
+        public void run() {
+            for (long i = 0; i < 10000; i++)
+                log.warn("whatever " + i);
+        }
+    }
+    
+    static Logger log = Logger.getLogger("LogMainThread");
+    
+    public static void main(String[] args) throws Exception {
+        Log4j2RedisTest.LogThread t = new Log4j2RedisTest.LogThread();
+        t.start();
+        
+        for (long i = 0; i < 10000; i++)
+            log.error("that's me " + i);
+    }
+}
---
+++ src/org/pbit/log4j2redis/RedisAppender.java
@@ -0,0 +1,76 @@
+package org.pbit.log4j2redis;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class RedisAppender extends AppenderSkeleton {
+    
+    static JedisPool pool;
+    static Map<Long, Jedis> map = new ConcurrentHashMap<Long, Jedis>();
+    
+    @Override
+    protected void append(LoggingEvent event) {
+        try {
+            StringBuilder id = new StringBuilder(InetAddress.getLocalHost().getHostName());
+            id.append(" - ");
+            id.append(ManagementFactory.getRuntimeMXBean().getName());
+            id.append(" - ");
+            id.append(Thread.currentThread().getId());
+            id.append(" - ");
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
+            id.append(dateFormat.format(date));
+            id.append(" - ");
+            id.append(event.getLevel().toString());
+            id.append(" - ");
+            id.append(UUID.randomUUID().toString());
+            getJedis().set(id.toString(), event.getRenderedMessage());
+        } catch (Exception e) {
+            //what to do? ignore? send back error - from log???
+        }
+    }
+
+    public void close() {
+    }
+    
+    public boolean requiresLayout() {
+        return false;
+    }
+    
+    public void setHost(String host) {
+        pool = new JedisPool(host);
+    }
+    
+    protected Jedis getJedis() {
+        //here, the connection pool must be cleaned,
+        //searching for non-existent threads and removing them from the map.
+        //otherwise, the map can really get pretty full.
+        //maybe this should be done in a parallel thread to immediately return from
+        //this call (oh, how I miss asynchronous message passing here...).
+        
+        Jedis j = map.get(Thread.currentThread().getId());
+        if (j == null) {
+            if (pool == null) {
+                setHost("localhost");
+            }
+            
+            j = pool.getResource();
+        }
+        
+        map.put(Thread.currentThread().getId(), j);
+        
+        return j;
+    }
+}
