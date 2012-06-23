114747f15dcf19e74cff58903c132b47955c487e
Leandro Silva <leandrodoze@gmail.com>
2012-06-23 17:47:51 -0300

    fix indentation

--- src/org/pbit/log4j2redis/RedisAppender.java
+++ src/org/pbit/log4j2redis/RedisAppender.java
@@ -24,67 +24,65 @@ public class RedisAppender extends AppenderSkeleton {
     private Jedis jedis;
     private String host = "localhost";
     private int port = 6379;
-    
+
     private Map<String, String> messages;
     private String localHostName;
     private String processName;
     private int MSetMax = 100;
-    
-	public void activateOptions() {
-		super.activateOptions();
 
-		jedis = new Jedis(host, port);
-		messages = new ConcurrentHashMap<String, String>();
+    public void activateOptions() {
+        super.activateOptions();
+
+        jedis = new Jedis(host, port);
+        messages = new ConcurrentHashMap<String, String>();
         try {
-			localHostName = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			localHostName = "localhost";
-		}
-        
-		processName = ManagementFactory.getRuntimeMXBean().getName();
-		
-		new Timer().schedule(new TimerTask() {
-			public void run() {
-			    
-			    //long begin = System.nanoTime();
-			    
-				Entry<String, String> message;
-				
-				int i = 0;
-				int msz = messages.size();
-				int sz = msz < MSetMax ? msz : MSetMax;
-				
-				byte[][] kv = new byte[sz * 2][];
-				for (Iterator<Entry<String, String>> it = messages.entrySet().iterator(); it.hasNext();) {
-				    message = it.next();
+            localHostName = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            localHostName = "localhost";
+        }
+
+        processName = ManagementFactory.getRuntimeMXBean().getName();
+
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                // long begin = System.nanoTime();
+
+                Entry<String, String> message;
+
+                int i = 0;
+                int msz = messages.size();
+                int sz = msz < MSetMax ? msz : MSetMax;
+
+                byte[][] kv = new byte[sz * 2][];
+                for (Iterator<Entry<String, String>> it = messages.entrySet().iterator(); it.hasNext();) {
+                    message = it.next();
                     kv[i] = SafeEncoder.encode(message.getKey());
                     kv[i + 1] = SafeEncoder.encode(message.getValue());
                     i += 2;
-					
-					it.remove();
-					
-					if (i == sz * 2) {
-					    jedis.mset(kv);
-					    msz -= sz;
+
+                    it.remove();
+
+                    if (i == sz * 2) {
+                        jedis.mset(kv);
+                        msz -= sz;
                         if (msz > 0) {
                             sz = msz < MSetMax ? msz : MSetMax;
                             kv = new byte[sz * 2][];
                             i = 0;
                         }
                     }
-				}
-				
-				//long expendHere = System.nanoTime() - begin;
-				
-		        //System.out.println("Expend here: " + expendHere + " ns");
-			}
-		}, 1000, 1000);
-		
-		
-		
-		/*new Timer().schedule(new TimerTask() {
-		 * left here for benchmark comparison, should be deleted when it's proven that the
-		 * version above indeed is x1.5 or even 2.0 faster 
+                }
+
+                // long expendHere = System.nanoTime() - begin;
+                // System.out.println("Expend here: " + expendHere + " ns");
+            }
+        }, 1000, 1000);
+
+        /*
+         left here for benchmark comparison, should be deleted when it's proven that the
+         version above indeed is x1.5 or even 2.0 faster
+         
+         new Timer().schedule(new TimerTask() {
             public void run() {
                 long begin = System.nanoTime();
                 
@@ -100,8 +98,9 @@ public class RedisAppender extends AppenderSkeleton {
                 
                 System.out.println("Expend here: " + expendHere + " ns");
             }
-        }, 5000, 5000);*/
-	}
+        }, 5000, 5000);
+        */
+    }
 
     protected void append(LoggingEvent event) {
         try {
@@ -116,35 +115,35 @@ public class RedisAppender extends AppenderSkeleton {
             id.append(event.getLevel());
             id.append(" - ");
             id.append(UUID.randomUUID());
-            
+
             messages.put(id.toString(), event.getRenderedMessage());
         } catch (Exception e) {
-            //what to do? ignore? send back error - from log???
+            // what to do? ignore? send back error - from log???
         }
     }
 
     public void close() {
     }
-    
-    public void setHost(String host) {
-		this.host = host;
-	}
 
-	public void setPort(String port) {
-		this.port = Integer.valueOf(port);
-	}
-	
-	public void setMsetmax(String m) {
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(String port) {
+        this.port = Integer.valueOf(port);
+    }
+
+    public void setMsetmax(String m) {
         this.MSetMax = Integer.valueOf(m);
     }
-	
-	public boolean requiresLayout() {
-    	return false;
+
+    public boolean requiresLayout() {
+        return false;
     }
-    
+
     private String now() {
-    	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
         return dateFormat.format(new Date());
     }
 }
